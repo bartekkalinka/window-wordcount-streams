166f1b8d509955949369bde90b2e850528c6bb07
Bartek Kalinka <bartkali@hotmail.com>
2016-12-25 13:55:18 +0100

    graphical frontend

--- src/main/resources/client/scripts/main.js
+++ src/main/resources/client/scripts/main.js
@@ -2,6 +2,8 @@
 
 var wsUri = "ws://" + window.location.hostname + ":" + window.location.port + "/window-streams?clientId=aaa";
 var websocket;
+var canvas = document.getElementById("canv");
+var ctx = canvas.getContext("2d");
 
 // INIT MODULE
 function init() {
@@ -28,10 +30,24 @@ function onOpen(evt) {
 function onClose(evt) {
 }
 function onMessage(evt) {
-   document.getElementById("output").innerHTML = evt.data;
+    var data = JSON.parse(evt.data);
+    draw(data);
 }
 function onError(evt) {
 }
 function doSend(message) {
+}
+function draw(data) {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < data.length; i++) {
+        var num = data[i][0];
+        var word = data[i][1];
+        ctx.fillStyle = "#8888FF";
+        ctx.fillRect(250, i * 60 + 60, num * 5, 59);
+        ctx.font = "20px Arial";
+        ctx.fillStyle = "#000000";
+        ctx.fillText(word, 250, i * 60 + 100);
+    }
 }
 init();
