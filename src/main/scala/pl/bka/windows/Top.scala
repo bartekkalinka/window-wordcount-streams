50e82be3c8eec4cbc37e81d4833a585a061e7db3
Bartek Kalinka <bartkali@hotmail.com>
2016-12-27 22:21:48 +0100

    window functions refactoring

--- src/main/scala/pl/bka/windows/Top.scala
+++ src/main/scala/pl/bka/windows/Top.scala
@@ -4,22 +4,27 @@ import akka.NotUsed
 import akka.stream.scaladsl.Flow
 
 object Top {
+  def sliding[A](windowSize: Int, windowFunction: Seq[String] => A): Flow[String, A, NotUsed] =
+    Flow[String].sliding(windowSize).map(windowFunction)
+
+  private def wordWitOccurenceFunction(minWordLength: Int): Seq[String] => (Int, String) = { window =>
+    val bestWordSeq =
+      window.filter(_.length >= minWordLength).groupBy(identity).values.toSeq.sortBy(_.length).reverse.head
+    (bestWordSeq.length, bestWordSeq.head)
+  }
+
   def wordWithOccurence(windowSize: Int, minWordLength: Int = 1): Flow[String, (Int, String), NotUsed] =
-    Flow[String]
-      .sliding(windowSize).map { window =>
-      val bestWordSeq =
-        window.filter(_.length >= minWordLength).groupBy(identity).values.toSeq.sortBy(_.length).reverse.head
-      (bestWordSeq.length, bestWordSeq.head)
-    }
+    sliding(windowSize, wordWitOccurenceFunction(minWordLength))
+
+  private def nwordsFunction(topWordsNum: Int, minWordLength: Int): Seq[String] => List[(Int, String)] = { window =>
+    window
+      .filter(_.length >= minWordLength)
+      .groupBy(identity).values.toSeq
+      .sortBy(_.length).reverse.take(topWordsNum).toList
+      .map { bestWordSeq => (bestWordSeq.length, bestWordSeq.head)}
+  }
 
   def nwords(windowSize: Int, topWordsNum: Int, minWordLength: Int = 1): Flow[String, List[(Int, String)], NotUsed] =
-    Flow[String]
-      .sliding(windowSize).map { window =>
-        window
-          .filter(_.length >= minWordLength)
-          .groupBy(identity).values.toSeq
-          .sortBy(_.length).reverse.take(topWordsNum).toList
-          .map { bestWordSeq => (bestWordSeq.length, bestWordSeq.head)}
-    }
+    sliding(windowSize, nwordsFunction(topWordsNum, minWordLength))
 }
 
