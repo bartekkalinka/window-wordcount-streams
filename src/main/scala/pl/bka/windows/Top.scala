a236d69ddf05fe75e2c69193004da5176146ffa4
Bartek Kalinka <bartkali@hotmail.com>
2016-12-28 19:29:59 +0100

    remove unused filters

--- src/main/scala/pl/bka/windows/Top.scala
+++ src/main/scala/pl/bka/windows/Top.scala
@@ -7,17 +7,8 @@ object Top {
   def nwordsSliding(windowSize: Int, topWordsNum: Int, minWordLength: Int = 1): Flow[String, List[(Int, String)], NotUsed] =
     sliding(windowSize, nwordsFunction(topWordsNum, minWordLength))
 
-  def wordSliding(windowSize: Int, minWordLength: Int = 1): Flow[String, (Int, String), NotUsed] =
-    sliding(windowSize, wordWitOccurenceFunction(minWordLength))
-
   private def sliding[A](windowSize: Int, windowFunction: Seq[String] => A): Flow[String, A, NotUsed] =
     Flow[String].sliding(windowSize).map(windowFunction)
-
-  private def wordWitOccurenceFunction(minWordLength: Int): Seq[String] => (Int, String) = { window =>
-    val bestWordSeq =
-      window.filter(_.length >= minWordLength).groupBy(identity).values.toSeq.sortBy(_.length).reverse.head
-    (bestWordSeq.length, bestWordSeq.head)
-  }
 
   private def nwordsFunction(topWordsNum: Int, minWordLength: Int): Seq[String] => List[(Int, String)] = { window =>
     window
