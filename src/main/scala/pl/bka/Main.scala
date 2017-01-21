f4c482589523dafae7e9b7beddf04925abdef80b
Bartek Kalinka <bartkali@hotmail.com>
2017-01-21 22:55:17 +0100

    messages stream

--- src/main/scala/pl/bka/Main.scala
+++ src/main/scala/pl/bka/Main.scala
@@ -9,9 +9,8 @@ import pl.bka.sources.{DebugSource, TextFileSource, TwitterSource}
 import pl.bka.windows.Top
 
 import scala.concurrent.duration._
-import spray.json._
 
-object Main extends JsonProtocols {
+object Main {
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
@@ -25,18 +24,18 @@ object Main extends JsonProtocols {
           WarmUpWindow.fakeWords(windowSize)
             .concat(TextFileSource.words("input3.txt", 10.millis))
             .via(Top.nwordsSliding(windowSize, 6, minWordLength(5)))
-            .via(Distinct.distinct(WindowWordCounts.zero))
+            .via(Distinct.distinct[Message](WindowWordCounts.zero))
         case "twitter" =>
           WarmUpWindow.fakeWords(windowSize)
             .concat(TwitterSource.source(Config(ConfigFactory.load())))
             .via(Top.nwordsSliding(windowSize, 6, minWordLength(1)))
-            .via(Distinct.distinct[WindowWordCounts](WindowWordCounts.zero))
+            .via(Distinct.distinct[Message](WindowWordCounts.zero))
       }
     args(1) match {
       case "web" =>
-        WebsocketDisplay(source.map(_.toJson.toString)).bind()
+        WebsocketDisplay(source.map(Message.toJson)).bind()
       case "stdout" =>
-        PrintlnDisplay(source.map(_.toJson.toString)).display()
+        PrintlnDisplay(source.map(Message.toJson)).display()
     }
   }
 }
--- src/main/scala/pl/bka/Model.scala
+++ src/main/scala/pl/bka/Model.scala
@@ -2,9 +2,17 @@ package pl.bka
 
 sealed trait Message
 
-case class WindowWordCounts(counts: List[(Int, String)])
+case class WindowWordCounts(counts: List[(Int, String)]) extends Message
 
 object WindowWordCounts {
   def zero: WindowWordCounts = WindowWordCounts(List((0, "")))
+}
+
+object Message extends JsonProtocols {
+  import spray.json._
+
+  def toJson(msg: Message): String = msg match {
+    case message: WindowWordCounts => message.toJson.toString
+  }
 }
 
