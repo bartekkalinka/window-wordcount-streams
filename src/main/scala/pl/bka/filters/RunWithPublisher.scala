606e158083a37cdbeb9dee33fb930214e8dd917d
Bartek Kalinka <bartkali@hotmail.com>
2017-01-27 12:20:31 +0100

    keep.both

--- src/main/scala/pl/bka/filters/RunWithPublisher.scala
+++ src/main/scala/pl/bka/filters/RunWithPublisher.scala
@@ -3,11 +3,11 @@ package pl.bka.filters
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.Materializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Keep, Sink, Source}
 
 object RunWithPublisher {
   def source[A, M](normal: Source[A, M])(implicit fm: Materializer, system: ActorSystem): (Source[A, NotUsed], M) = {
-    val (normalMat, publisher) = normal.toMat(Sink.asPublisher(fanout = true))((a, b) => (a, b)).run
+    val (normalMat, publisher) = normal.toMat(Sink.asPublisher(fanout = true))(Keep.both).run
     (Source.fromPublisher(publisher), normalMat)
   }
 }
